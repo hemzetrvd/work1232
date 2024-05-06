97a1410c3e87ba2bd40d7275b2af032a44314f43
Hemze <hmztarverdiyev@gmail.com>
2024-05-07 00:12:27 +0400

    name

--- work12/Program.cs
+++ work12/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("YaşınI gir:");
+            int yas = int.Parse(Console.ReadLine());
+
+            if (yas >= 1 && yas <= 6)
+            {
+                Console.WriteLine("uşaq");
+            }
+            else if (yas >= 7 && yas <= 17)
+            {
+                Console.WriteLine("şagird");
+            }
+            else if (yas >= 18 && yas <= 35)
+            {
+                Console.WriteLine("gənc");
+            }
+            else if (yas >= 36 && yas <= 63)
+            {
+                Console.WriteLine("yetkinlik");
+            }
+            else if (yas == 64)
+            {
+                Console.WriteLine("təqaüdçü");
+            }
+            else
+            {
+                Console.WriteLine("Düzgün yaş yaz.");
+            }
+
+
         }
     }
+
+
 }
